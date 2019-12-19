25064d3d58feddb90338287dc9a35eab09c92ae3
Yakhnitsa <transpmailbox@gmail.com>
2019-12-19 22:03:24 +0200

    Add pages via router-vue

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -3,19 +3,20 @@
     <v-bottom-navigation
             v-model="activeItem">
         <v-btn flat color="primary"
-               value="top">
+               value="top"
+               :to="{ path: '/'}">
             <span>Top Stories</span>
             <v-icon>mdi-thumb-up-outline</v-icon>
         </v-btn>
 
         <v-btn color="primary"
-               value="code">
+               value="code" :to="{name: 'code-examples'}">
             <span>Code Examples</span>
             <v-icon>mdi-code-tags</v-icon>
         </v-btn>
 
         <v-btn flat color="primary"
-               value="favorites">
+               value="favorites" :to="{name: 'my-favorites'}">
             <span>Favorites</span>
             <v-icon>mdi-heart-outline</v-icon>
         </v-btn>
--- src/views/MyFavorites.vue
+++ src/views/MyFavorites.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <h1 class="headline">[[My Favrites]]</h1>
+        <h1 class="headline">[[My Favorites]]</h1>
         Content is coming to this mobile app very soon!
     </v-container>
 </template>
