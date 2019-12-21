48e83d6bc3976e8aa9011dcc864c59e98b7f4802
Yuriy work-pc <transpmailbox@gmail.com>
2019-12-21 15:16:16 +0200

    Added hardcoded component to topstories

--- src/views/TopStories.vue
+++ src/views/TopStories.vue
@@ -2,12 +2,17 @@
     <v-container>
         <h1 class="headline">[[Top Stories]]</h1>
         Content is coming to this mobile app very soon!
+        <news-list></news-list>
     </v-container>
 </template>
 
 <script>
+    import NewsList from '../components/NewsList.vue'
     export default {
-        name: "TopStories.vue"
+        name: "TopStories.vue",
+        components:{
+            NewsList
+        }
     }
 </script>
 
